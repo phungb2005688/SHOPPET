c8b1dcfae624f1145c51556b959f8fc8b4bb5ffc
phungb2005688 <122144847+phungb2005688@users.noreply.github.com>
2024-05-01 12:56:55 +0700

    second commit

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -12,7 +12,6 @@ import Message from '../components/Message';
 // import Paginate from '../components/Paginate';
 import ProductCarousel from '../components/ProductCarousel';
 import Meta from '../components/Meta';
-// import { useEffect } from 'react';
 
 const HomeScreen = () => {
   const { pageNumber, keyword } = useParams();
